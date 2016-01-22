83dd01451127e5a8e0bdd1c4732b1af9ff8f8de4
leichunfeng <307213080@qq.com>
2016-01-22 10:23:40 +0800

    Project and dependencies set up

---
+++ RGB/RGB.h
@@ -0,0 +1,19 @@
+//
+//  RGB.h
+//  RGB
+//
+//  Created by leichunfeng on 16/1/21.
+//  Copyright © 2016年 leichunfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RGB.
+FOUNDATION_EXPORT double RGBVersionNumber;
+
+//! Project version string for RGB.
+FOUNDATION_EXPORT const unsigned char RGBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGB/PublicHeader.h>
+
+
